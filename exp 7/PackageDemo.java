a6efcfb66f89c297cd1f2935b73dfea832fd81ad
Prithvi24csu245 <24csu345@ncuindia.edu>
2025-11-17 10:22:22 +0530

    Update PackageDemo.java

--- exp 7/PackageDemo.java
+++ exp 7/PackageDemo.java
@@ -3,4 +3,6 @@ package exercises.java;
 public class PackageDemo {
     public void display() {
         System.out.println("PackageDemo executed");
+        System.out.print(" Prithviraj 24csu345");
     }
+
