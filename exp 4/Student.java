9431e980a5ba3735f55f8f8103dd90afa6c28049
Prithvi24csu245 <24csu345@ncuindia.edu>
2025-11-17 10:18:36 +0530

    Update output format in Student class

--- exp 4/Student.java
+++ exp 4/Student.java
@@ -6,8 +6,9 @@ class Student {
         Student s1 = new Student();
         s1.name = "John";
         s1.roll_no = 2;
-        System.out.println("Name: Monsih Rao \nroll no.: 24csu321 ");
+        System.out.print(" Prithviraj 24csu345");
         System.out.println("Name: " + s1.name);
         System.out.println("Roll No: " + s1.roll_no);
     }
 }
+
